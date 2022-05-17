767e36ff1312200b11d4ef22baa79c5d5b2df4b0
Yusuke Mizoguchi <33048729+swiftfile@users.noreply.github.com>
2022-05-17 22:02:31 +0900

    Update setup01.sh

--- setup01.sh
+++ setup01.sh
@@ -20,3 +20,8 @@ sudo apt install -y gnome-shell-extension-system-monitor
 sudo snap install -y remmina
 sudo apt install -y nscd
 sudo systemctl restart snapd.service
+sudo snap install clion --classic
+sudo apt install guake -y
+wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+sudo dpkg -i gitkraken-amd64.deb
+rm gitkraken-amd64.deb
